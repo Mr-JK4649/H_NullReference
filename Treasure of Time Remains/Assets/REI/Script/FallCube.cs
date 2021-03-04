055a737c99d870afcce26f95d933db0af1e4f187
rei0531 <poripuropiren12@gmail.com>
2021-03-04 15:32:02 +0900

    メインシーンに瓦礫落ちるシェーダー追加

---
+++ Treasure of Time Remains/Assets/REI/Script/FallCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallCube : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit");
+            this.transform.parent.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
