faf8128b2d9bd8fe2cdb9c1f74ea3d8240750ece
星矢池間1203 <seiya-ikema-19h@stu.kbc.ac.jp>
2021-03-12 15:15:58 +0900

    fallcubeのスクリプト改善してください(切実)

--- Treasure of Time Remains/Assets/REI/Script/FallCube.cs
+++ Treasure of Time Remains/Assets/REI/Script/FallCube.cs
@@ -1,14 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Chronos;
 
+[RequireComponent(typeof(Rigidbody))]
 public class FallCube : MonoBehaviour
 {
+    GameObject TimeKeeper;
+    GlobalClock[] script = new GlobalClock[2];
+    bool IsFall = false;
+    void Start()
+    {
+        TimeKeeper = GameObject.Find("Timekeeper");
+        script = TimeKeeper.GetComponents<GlobalClock>();
+    }
+
+    void Update()
+    {
+        if (script[0].localTimeScale == 1 && script[1].localTimeScale == 1 && IsFall)//能力使ってないとき
+        {
+           this.transform.parent.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+        else if(IsFall == true)
+        {
+            this.transform.parent.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Hit");
+            IsFall = true;
             this.transform.parent.gameObject.GetComponent<Rigidbody>().useGravity = true;
         }
     }
--- Treasure of Time Remains/Assets/REI/Script/RotationCube.cs
+++ Treasure of Time Remains/Assets/REI/Script/RotationCube.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Chronos;
 
+//[RequireComponent(typeof(Rigidbody))]
 public class RotationCube : MonoBehaviour
 {
-    public GlobalClock _GlobalClock;
     GameObject TimeKeeper;
     GlobalClock[] script = new GlobalClock[2];
     public float x;
@@ -21,11 +21,15 @@ public class RotationCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (script[1].timeScale == 1)
+        if (script[0].timeScale == 1 && script[1].localTimeScale == 1)
         {
             transform.Rotate(new Vector3(x, y, z));
         }
 
-
+        //if(script[1].timeScale == 0)
+        //{
+        //    this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        //}else
+        //    this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
     }
 }
