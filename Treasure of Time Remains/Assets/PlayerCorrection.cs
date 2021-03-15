c98b6f62dc0878d694cf56eb43f2b9a8ac943cb3
Mr-JK4649 <ryouga>
2021-03-15 12:54:39 +0900

    ラスカル？

---
+++ Treasure of Time Remains/Assets/PlayerCorrection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCorrection : MonoBehaviour
+{
+    private GameObject me;
+    private Rigidbody me_r;
+
+    [SerializeField] private Vector3 accelNum;
+
+    private void Start()
+    {
+        me = this.gameObject;
+        me_r = me.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        me_r.velocity += accelNum;
+    }
+}
