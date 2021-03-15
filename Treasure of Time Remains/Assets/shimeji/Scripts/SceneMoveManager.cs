8ec06c36671d4ca496c3670d3ca0cfcb7fb28f26
Mr-JK4649 <ryouga>
2021-03-15 20:12:34 +0900

    シーン遷移のスクリプト変更

--- Treasure of Time Remains/Assets/shimeji/Scripts/SceneMoveManager.cs
+++ Treasure of Time Remains/Assets/shimeji/Scripts/SceneMoveManager.cs
@@ -3,12 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneMoveManager : MonoBehaviour
 {
+    private enum SCENE { 
+        TITLE,
+        MAIN,
+        RESULT
+    }
+
+    [SerializeField] private SCENE currentScene;
+
     [SerializeField] private string sceneName;
 
     private void Update()
     {
-        if (Input.GetButtonDown("Submit"))
-            SceneManager.LoadScene(sceneName);
+        
+
+        switch (currentScene) {
+            case SCENE.TITLE:
+            case SCENE.RESULT:
+                if (Input.GetButtonDown("Submit"))
+                    SceneManager.LoadScene(sceneName);
+                break;
+
+            case SCENE.MAIN:
+                SceneManager.LoadScene(sceneName);
+                break;
+        }
 
     }
 }
