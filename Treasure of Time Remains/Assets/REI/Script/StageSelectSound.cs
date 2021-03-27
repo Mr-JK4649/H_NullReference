3f28df75df44b9a1792e8586e1cd2fb428b586cd
rei0531 <poripuropiren12@gmail.com>
2021-03-27 12:56:37 +0900

    ステージ選択画面の時メニュー内同いたら音が鳴るように スティックはまだ

---
+++ Treasure of Time Remains/Assets/REI/Script/StageSelectSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectSound : MonoBehaviour
+{
+
+    public AudioClip MenuMove;
+    public AudioClip MenuOK;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        //Componentを取得
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        // 左右
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            //音(MenuMove)を鳴らす
+            audioSource.PlayOneShot(MenuMove);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            //音(MenuMove)を鳴らす
+            audioSource.PlayOneShot(MenuOK);
+        }
+
+    }
+}
