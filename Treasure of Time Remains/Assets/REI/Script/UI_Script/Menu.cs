16573115ddd33a8e90440c4d26b7aa7a943a9b96
rei0531 <poripuropiren12@gmail.com>
2021-03-29 15:54:19 +0900

    ステージ３しか選べないようにした

--- Treasure of Time Remains/Assets/REI/Script/UI_Script/Menu.cs
+++ Treasure of Time Remains/Assets/REI/Script/UI_Script/Menu.cs
@@ -9,15 +9,18 @@ public class Menu : MonoBehaviour
 	Button tutorial_button;
 	Button stage1_button;
 	Button stage2_button;
+    Button stage3_button;
 
-	void Start()
+    void Start()
 	{
         // ボタンコンポーネントの取得
-        tutorial_button = GameObject.Find("/Canvas/TutorialButton").GetComponent<Button>();
-        stage1_button = GameObject.Find("/Canvas/Stage1Botton").GetComponent<Button>();
-        stage2_button = GameObject.Find("/Canvas/Stage2Button").GetComponent<Button>();
+        //tutorial_button = GameObject.Find("/Canvas/TutorialButton").GetComponent<Button>();
+        //stage1_button = GameObject.Find("/Canvas/Stage1Botton").GetComponent<Button>();
+        //stage2_button = GameObject.Find("/Canvas/Stage2Button").GetComponent<Button>();
+        stage3_button = GameObject.Find("/Canvas/Stage3Button").GetComponent<Button>();
 
         // 最初に選択状態にしたいボタンの設定
-        tutorial_button.Select();
+        //tutorial_button.Select();
+        stage3_button.Select();
     }
 }
