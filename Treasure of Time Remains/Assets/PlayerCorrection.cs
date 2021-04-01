4f96495b24b88aec5055f5cee29643fa60dc82a0
Mr-JK4649 <ryouga>
2021-04-01 16:56:53 +0900

    きゃらのやつ

--- Treasure of Time Remains/Assets/PlayerCorrection.cs
+++ Treasure of Time Remains/Assets/PlayerCorrection.cs
@@ -7,17 +7,25 @@ public class PlayerCorrection : MonoBehaviour
     private GameObject me;
     private Rigidbody me_r;
 
+    private float oriXpos;
+
     [SerializeField] private Vector3 accelNum;
 
     private void Start()
     {
         me = this.gameObject;
         me_r = me.GetComponent<Rigidbody>();
+
+        oriXpos = me.transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         me_r.velocity += accelNum;
+
+        float y = me.transform.position.y;
+        float z = me.transform.position.z;
+        me.transform.position = new Vector3(oriXpos, y, z);
     }
 }
