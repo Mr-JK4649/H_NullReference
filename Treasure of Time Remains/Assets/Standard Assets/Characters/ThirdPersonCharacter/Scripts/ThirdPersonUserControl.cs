6fe9fecaa8d7c8e7005d47c1a9e27227cfa5beac
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-04-07 12:56:01 +0900

    charmotion修正

--- Treasure of Time Remains/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Treasure of Time Remains/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -16,6 +16,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private bool _animetion_jump_flg;
         [SerializeField] private Animator ethan;
 
+        //ZAHA編集 4月7日 ariajump追加
+        AriaJumpmotion ariajump;
+        //ZAHA編集 4月7日 aria追加
+        [SerializeField]
+        GameObject aria;
+
         private void Start()
         {
             // get the transform of the main camera
@@ -32,6 +38,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
             // get the third person character ( this should never be null due to require component )
             m_Character = GetComponent<ThirdPersonCharacter>();
+            
+            //ZAHA編集 4月7日 追加
+            ariajump = aria.GetComponent<AriaJumpmotion>();
         }
 
 
@@ -53,8 +62,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 m_Character.m_Animator.SetBool("Jumpflg", false);
             }
 
-            //キーが押されてない間かつ地面にいる時
-            if ((!_animetion_jump_flg)&&(m_Character.m_IsGrounded)){
+            //キーが押されてない間かつ地面にいる時エリアに入ったら
+            if ((!_animetion_jump_flg)&&(m_Character.m_IsGrounded) && (ariajump.aria_check))
+            {
                 m_Character.z_rb.velocity = Vector3.zero;
             }
             /////////////////////////////////////////////////////////////////////////////////
