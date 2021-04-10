1a8994785b13a3737208b0692fa8cd6818d03c53
rei0531 <poripuropiren12@gmail.com>
2021-04-10 14:36:58 +0900

    シネマシンおパッケージインストール

---
+++ Treasure of Time Remains/Assets/REI/Script/Example.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    public CameraShake shake;
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    shake.Shake(0.25f, 0.25f);
+        //}
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Fall")
+        {
+            shake.Shake(0.25f, 0.25f);
+        }
+
+    }
+}
