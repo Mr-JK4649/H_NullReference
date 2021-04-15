5345274a84b2d2182faeabacf4fbf619977119c5
yazirusi <hukumuraise072@gmail.com>
2021-04-15 17:13:38 +0900

    チュートリアル９０％

---
+++ Treasure of Time Remains/Assets/sei/test3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test3 : MonoBehaviour
+{
+
+    // 初期化
+    void Start()
+    {
+        GameObject obj = (GameObject)Resources.Load("Cube");
+
+        // プレハブを元にオブジェクトを生成する
+        GameObject instance = (GameObject)Instantiate(obj,
+                                                      new Vector3(5.0f, 0.0f, 0.0f),
+                                                      Quaternion.identity);
+    }
+}
