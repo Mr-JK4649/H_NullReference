d917b40838b8a5808bbc85810aabab78c7a951f3
星矢池間1203 <seiya-ikema-19h@stu.kbc.ac.jp>
2021-04-28 15:39:34 +0900

    aa

---
+++ Treasure of Time Remains/Assets/Seiya/Script/Naname_FloatObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chronos;
+
+public class Naname_FloatObject : MonoBehaviour
+{
+    public int limit = 100; //移動時間
+    int counter = 0; //これがリミットより大きくなると移動止める
+    //bool IsArea　= false;
+    Naname IsArea;
+    [SerializeField]GameObject Area;
+    public float movespeed_x = 0f;
+    public float movespeed_y = 0f;
+    public float movespeed_z = 0f;
+
+    GameObject TimeKeeper;
+    GlobalClock[] script = new GlobalClock[2];
+
+    void Start()
+    {
+        TimeKeeper = GameObject.Find("Timekeeper");
+        script = TimeKeeper.GetComponents<GlobalClock>();
+        IsArea= Area.GetComponent<Naname>();
+    }
+
+    private void Update()
+    {
+        if (script[0].timeScale == 1 && script[1].localTimeScale == 1 && Time.timeScale != 0 && IsArea.push)
+        {
+            Vector3 p = new Vector3(movespeed_x, movespeed_y, movespeed_z);
+            transform.Translate(p);
+            counter++;
+        }
+
+        Debug.Log(IsArea.push);
+        //countがlimitになれば止める
+        if (counter == limit)
+        {
+            counter = 0;
+            movespeed_z = 0;
+        }
+    }
+
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")//プレイヤーにあたったらIsAeraをonにするから移動する
+    //    {
+    //        IsArea.push = true;
+
+    //    }
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Floor")//プレイヤーにあたったらIsAeraをonにするから移動する
+        {
+            movespeed_z = 0;
+
+        }
+    }
+}
---
+++ Treasure of Time Remains/Assets/Seiya/Script/PushHari.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushHari : MonoBehaviour
+{
+    Naname naname_aria;
+    Rigidbody rb;
+    bool pushflg = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pushflg)
+        {
+            rb.velocity = new Vector3(0f, -10f, -10f);
+        }
+        //if (naname_aria.push)
+        //{
+        //    rb.velocity = new Vector3(1f, 1f, 0f);
+        //}   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Floor")
+        {
+            pushflg = false;
+        }
+    }
+}
