242fccc7ca0b7fc304015c5669315bc2e11f15c0
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-04-28 23:17:20 +0900

    (push内容) 04/28 担当者ZAHA ・キャラクターの足元に土煙のパーティクルを導入。 ・マテリアルフォルダ作成 ・パーティクルフォルダ作成 ・Textureフォルダ作成 ・パーティクルスクリプト追加

---
+++ Treasure of Time Remains/Assets/Seiya/Script/particleScript/particle_FootSmoke.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class particle_FootSmoke : MonoBehaviour
+    {
+        ThirdPersonCharacter m_charcter;
+        [SerializeField]
+        GameObject m_player;
+        private ParticleSystem footsmoke;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            m_charcter = m_player.GetComponent<ThirdPersonCharacter>();
+            footsmoke = GetComponent<ParticleSystem>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (m_charcter.m_IsGrounded)
+            {
+                footsmoke.Play();
+            }
+            else
+            {
+                footsmoke.Stop();
+
+            }
+        }
+    }
+}
