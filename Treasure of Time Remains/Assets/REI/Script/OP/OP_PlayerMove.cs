f222dff95470b672540e99dbe644172d188956d6
rei0531 <poripuropiren12@gmail.com>
2021-05-02 10:35:28 +0900

    OPに動きおちょっと追加

---
+++ Treasure of Time Remains/Assets/REI/Script/OP/OP_PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OP_PlayerMove : MonoBehaviour
+{
+    float count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+        if (count <= 7)
+        {
+        transform.Translate(0f, 0f, 0.001f);
+
+        }
+    }
+}
