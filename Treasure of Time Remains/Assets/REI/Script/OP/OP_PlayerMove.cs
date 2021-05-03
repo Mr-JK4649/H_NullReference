a6191d8227ea709a5d6fc7cf7e52ceeaa9926bc2
rei0531 <poripuropiren12@gmail.com>
2021-05-03 13:25:28 +0900

    追加いわすれ

--- Treasure of Time Remains/Assets/REI/Script/OP/OP_PlayerMove.cs
+++ Treasure of Time Remains/Assets/REI/Script/OP/OP_PlayerMove.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class OP_PlayerMove : MonoBehaviour
 {
     float count;
+    public GameObject Mojiban;
+    public GameObject cam;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GameObject Mojiban = this.transform.Find("ClockCanvas").gameObject;
     }
 
     // Update is called once per frame
@@ -18,8 +20,17 @@ public class OP_PlayerMove : MonoBehaviour
         count += Time.deltaTime;
         if (count <= 7)
         {
-        transform.Translate(0f, 0f, 0.001f);
+            transform.Translate(0f, 0f, 0.0015f);
 
+        }
+        else if (count > 11.5)
+        {
+            Mojiban.SetActive(true);
+            cam.SetActive(true);
+        }
+        else
+        {
+            cam.SetActive(false);
         }
     }
 }
