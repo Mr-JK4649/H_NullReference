f60df5b83f70ed043eebb7cbf292a663d4aeca6e
rei0531 <poripuropiren12@gmail.com>
2021-05-03 13:25:16 +0900

    オープニング大体の形完成

---
+++ Treasure of Time Remains/Assets/REI/Script/OP/OP_Orb_Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OP_Orb_Move : MonoBehaviour
+{
+    float count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+        if (count >= 8.3 && count <= 11)
+        {
+            transform.Translate(0f, 0f, 0.0008f);
+
+        }
+        else if (count > 10.5)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
