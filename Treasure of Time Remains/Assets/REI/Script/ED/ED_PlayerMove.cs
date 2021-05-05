437766a030cb2b318990309088753960b957eebd
rei0531 <poripuropiren12@gmail.com>
2021-05-05 22:28:39 +0900

    ED途中

---
+++ Treasure of Time Remains/Assets/REI/Script/ED/ED_PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ED_PlayerMove : MonoBehaviour
+{
+    float count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+        if (count >= 22)
+        {
+            transform.Translate(-0.002f, 0f, 0.007f);
+        }
+    }
+}
