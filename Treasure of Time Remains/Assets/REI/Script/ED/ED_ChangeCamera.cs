fe3c51a0a56a067b3730b21ef53631173da81e9a
rei0531 <poripuropiren12@gmail.com>
2021-05-06 13:37:13 +0900

    EDほぼOK

--- Treasure of Time Remains/Assets/REI/Script/ED/ED_ChangeCamera.cs
+++ Treasure of Time Remains/Assets/REI/Script/ED/ED_ChangeCamera.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 public class ED_ChangeCamera : MonoBehaviour
 {
     public GameObject Cam;
+    public CameraShake Cam1;
+    float count;
+    public GameObject Fade;
+    public FadeController FadeCon;
+    GameObject panel;
     // Start is called before the first frame update
     void Start()
     {
 
+        Application.targetFrameRate = 60;
         Invoke("ChangeCam", 19f);//２0秒後にシーン切りかえ
+        Fade = GameObject.Find("FadeCanvas/Panel");
+        FadeCon = Fade.GetComponent<FadeController>();
+        panel = GameObject.Find("Canvas");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        count += Time.deltaTime;
+        if (count >= 19.2)
+        {
+        Cam1.Shake(0.0001f, 0.0001f);
+        }
+        if (count >= 25)
+        {
+            FadeCon.enabled = true;
+            panel.SetActive(false);
+        }
     }
 
     void ChangeCam()
     {
         Cam.SetActive(true);
+        Cam1.Shake(0.0001f, 0.0001f);
+
+        Debug.Log("ここまではくる");
     }
 }
---
+++ Treasure of Time Remains/Assets/REI/Script/ED/ED_Player_Sound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ED_Player_Sound : MonoBehaviour
+{
+    public AudioClip sound1;
+    public AudioClip sound2;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Invoke("Sound",1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Sound()
+    {
+        audioSource.PlayOneShot(sound1);
+        audioSource.PlayOneShot(sound2);
+
+    }
+}
