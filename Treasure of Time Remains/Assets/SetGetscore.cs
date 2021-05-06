2f524e9ff33e84679de6e0c9f2c82b12ec51d4be
安藤翼 <tubasa-ando-19h@stu.kbc.ac.jp>
2021-05-06 16:30:57 +0900

    リザルト修正

--- Treasure of Time Remains/Assets/SetGetscore.cs
+++ Treasure of Time Remains/Assets/SetGetscore.cs
@@ -20,5 +20,8 @@ public class SetGetscore : MonoBehaviour
         hscore.text = hs.ToString();
         result.text = "Stage" + num.ToString() + "RESULT";
     }
-
+    private void Update()
+    {
+        Debug.Log(score);
+    }
 }
