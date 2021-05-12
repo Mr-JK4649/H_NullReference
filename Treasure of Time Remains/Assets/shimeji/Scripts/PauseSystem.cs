7c59ded782189eda70186cbbfc0e7b6256bf07f9
Mr-JK4649 <ryouga>
2021-05-12 13:53:31 +0900

    いらないスクリプト消したのと、メニューを再度開いたときに選択が消されるのを修正

--- Treasure of Time Remains/Assets/shimeji/Scripts/PauseSystem.cs
+++ Treasure of Time Remains/Assets/shimeji/Scripts/PauseSystem.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PauseSystem : MonoBehaviour
 {
     [SerializeField] private GameObject pausePanel;
 
-    [SerializeField] private Button[] button;
+    //[SerializeField] private Button firstSelectButton;
+    [SerializeField] private GameObject selectButton;
 
     void Update()
     {
+
         if (Input.GetButtonDown("Cont_Start"))
         {
             Time.timeScale = 1 - Time.timeScale;
+
             if (Time.timeScale == 0) {
                 pausePanel.SetActive(true);
-                button[0].Select();
+                EventSystem.current.SetSelectedGameObject(selectButton);
             }
+            
         }
 
-        if (Time.timeScale > 0) pausePanel.SetActive(false);
+        if (Time.timeScale > 0)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            pausePanel.SetActive(false);
+        }
+
     }
 }
--- Treasure of Time Remains/Assets/shimeji/Scripts/ScoreManager.cs
+++ Treasure of Time Remains/Assets/shimeji/Scripts/ScoreManager.cs
@@ -2,8 +2,9 @@
 
 public class ScoreManager : SingletonMonoBehaviour<ScoreManager>
 {
-    public int score;
-    public int[] stage_Highscore = new int[4];
-    public int StageNum;
-    
+    public int score;                           //スコア
+    public int[] stage_Highscore = new int[4];  //そのステージのハイスコア
+    public int StageNum;                        //プレイしたステージの番号
+    public int keptAbility;                     //クリア時に保持してた能力残数
+    public int retries;                         //リトライ回数
 }
