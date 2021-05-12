110d697b8d8681eec20b5b18237869e0692b6b1e
rei0531 <poripuropiren12@gmail.com>
2021-05-12 15:05:44 +0900

    タイトル

--- Treasure of Time Remains/Assets/REI/Script/Title/TitleScript.cs
+++ Treasure of Time Remains/Assets/REI/Script/Title/TitleScript.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScript : MonoBehaviour
 {
     float count;
+    float flg = 0;
+    public GameObject fade;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +17,18 @@ public class TitleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump")) SceneMove();
+        if (Input.GetButtonDown("Jump"))
+        {
+            flg = 1;
+            fade.SetActive(true);
+        }
+
+        if (flg == 1) count += Time.deltaTime;
+
+        if (count >= 2) SceneMove();
     }
     void SceneMove()
     {
-
+        SceneManager.LoadScene("StageSelect");
     }
 }
