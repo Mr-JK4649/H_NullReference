3cd39e00c9cd8d94a2f9bc3838e52f7e6ef33e14
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-05-13 14:26:07 +0900

    時計時間ゲージ素材フォルダ作成し、ZAHAフォルダにインポートします。

---
+++ Treasure of Time Remains/Assets/ZAHA/ClockTimeGauge/Script/ClockGauge.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockGauge : MonoBehaviour
+{
+    public bool isActive;//能力がアクティブか
+    private float limit = 60f;
+  
+    public float time;
+
+    private void Awake()
+    {
+        isActive = false;
+    }
+
+    public void UpdateClockGauge(float timeNum)
+    {
+
+        if (!isActive)
+        {
+            isActive = true;
+        }
+
+        if (isActive)
+        {
+            time += Time.deltaTime;
+
+            if(time >= 60) { TimeEnd(); time = 60; }
+        }
+
+        Debug.Log(timeNum);
+    }
+
+    public void TimeEnd()
+    {
+        isActive = false;
+    }
+
+    public void Recover(float rtime)
+    {
+        time -= rtime;
+        if (limit <= time)
+        {
+            time = limit;
+        }
+    }
+}
---
+++ Treasure of Time Remains/Assets/ZAHA/ClockTimeGauge/Script/ClockMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ClockMove : MonoBehaviour
+{
+    const float angle360 = 360;
+    Image second_clockimage;
+
+    [SerializeField] GameObject second;
+    [SerializeField] GameObject clockGauge_ogj;
+    ClockGauge clockGauge;
+
+    float second_timeangle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clockGauge = clockGauge_ogj.GetComponent<ClockGauge>();
+        second_clockimage = GetComponent<Image>();
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ////時間計算処理
+        //nowtime += Time.deltaTime; //加算
+
+        if (second_clockimage.fillAmount > 0)
+        {
+            //秒針を左回りに回転
+            second.transform.eulerAngles = new Vector3(0f, 0f, (float)clockGauge.time / 60 * angle360);
+        }
+
+        //秒針角度生成
+        second_timeangle = (float)clockGauge.time / 60 * angle360;
+
+
+        //エモート表示処理
+        second_clockimage.fillAmount = 1 - (second_timeangle / 360);
+
+
+    }
+}
