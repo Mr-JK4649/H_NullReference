0b0e5ace1c55097d52a228e84df7d889da15029c
rei0531 <poripuropiren12@gmail.com>
2021-05-13 15:06:08 +0900

    タイトルOP変更

--- Treasure of Time Remains/Assets/REI/Script/Title/TitleScript.cs
+++ Treasure of Time Remains/Assets/REI/Script/Title/TitleScript.cs
@@ -8,22 +8,32 @@ public class TitleScript : MonoBehaviour
     float count;
     float flg = 0;
     public GameObject fade;
+
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (flg == 0 && Input.GetButtonDown("Jump"))
         {
             flg = 1;
             fade.SetActive(true);
+            audioSource.PlayOneShot(sound1);
         }
 
-        if (flg == 1) count += Time.deltaTime;
+        if (flg == 1)
+        {
+            count += Time.deltaTime;
+        }
 
         if (count >= 2) SceneMove();
     }
