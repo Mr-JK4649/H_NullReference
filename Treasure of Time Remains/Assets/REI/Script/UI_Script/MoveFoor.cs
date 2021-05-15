5c9bf1be0bf0c3ffeb82fda862ae9c74e47b72d9
rei0531 <poripuropiren12@gmail.com>
2021-05-15 14:17:28 +0900

    タイトル修正

---
+++ Treasure of Time Remains/Assets/REI/Script/UI_Script/MoveFoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFoor : MonoBehaviour
+{
+    float z;
+    Vector3 tmp;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 tmp = transform.root.gameObject.transform.position;
+        Invoke("Move",2f);
+        Debug.Log("えんたー");
+    }
+    void Move()
+    {
+        Debug.Log("むーぶ");
+        //tmp.z += tmp.z * 2f;
+        transform.root.position = new Vector3(0f, 0f, tmp.z * 2f);
+    }
+}
