1c495132dbb729022250ec634351271b7a61bf02
安藤翼 <tubasa-ando-19h@stu.kbc.ac.jp>
2021-05-17 15:04:18 +0900

    フォントボールドからノーマルに変更、難易度スター設置、ブランコ良化

--- Treasure of Time Remains/Assets/SelectSystem.cs
+++ Treasure of Time Remains/Assets/SelectSystem.cs
@@ -6,15 +6,41 @@ public class SelectSystem : MonoBehaviour
 {
     [SerializeField] private EventSystem eSys;
     [SerializeField] private Text highScore;
+    [SerializeField] private Text Level;
     [SerializeField] private GameObject[] buttons;
 
     private void FixedUpdate()
     {
         GameObject go = eSys.currentSelectedGameObject.gameObject;
 
-        for (int i = 0; i < buttons.Length - 1; i++) {
-            if (go == buttons[i]) highScore.text = "ハイスコア：\n　　　" + ScoreManager.Instance.stage_Highscore[i].ToString();
+        for (int i = 0; i < buttons.Length - 1; i++)
+        {
+            if (go == buttons[0])
+            {
+                Level.text = "★☆☆☆";
+            }
+            else if (go == buttons[1])
+            {
+                Level.text = "★★☆☆";
+            }
+            else if (go == buttons[2])
+            {
+                Level.text = "★★★☆";
+            }
+            else if (go == buttons[3])
+            {
+                Level.text = "★★★★";
+            }
+            else if (go == buttons[4])
+            {
+                Level.text = "";
+            }
         }
+
+    for (int i = 0; i < buttons.Length - 1; i++) 
+            {
+                if (go == buttons[i]) highScore.text = "ハイスコア：\n　　　" + ScoreManager.Instance.stage_Highscore[i].ToString();
+            }
         if (go == buttons[buttons.Length-1]) highScore.text = "";
 
         Debug.Log(buttons.Length);
