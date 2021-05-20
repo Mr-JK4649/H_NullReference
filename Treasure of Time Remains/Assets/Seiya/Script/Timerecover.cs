12f6499c690e9762730f6f9bbfa3de1d2ad586bd
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-05-20 16:55:56 +0900

    ClockGaugeAset

--- Treasure of Time Remains/Assets/Seiya/Script/Timerecover.cs
+++ Treasure of Time Remains/Assets/Seiya/Script/Timerecover.cs
@@ -3,6 +3,11 @@ using System;
 
 public class Timerecover : MonoBehaviour
 {
+    [SerializeField]
+    GameObject objeffect;
+
+    Sample effect;
+
     [SerializeField] private TimeGauge timeGauge;
     [SerializeField] private ClockGauge clockgauge;
 
@@ -17,12 +22,17 @@ public class Timerecover : MonoBehaviour
 
     private const float Recovery = 0.2f;
 
+    private void Start()
+    {
+        effect = objeffect.GetComponent<Sample>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Recover")
         {
             clockgauge.Recover(Recovery);
             timeGauge.Recover(Recovery);
+            effect.effectStatus = true;
             ScoreManager.Instance.orb += 1;
             Instantiate(p_Text, other.transform.position, Quaternion.identity, p_T_Parent);
             if (isDebug)
@@ -33,5 +43,6 @@ public class Timerecover : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+
 
 }
