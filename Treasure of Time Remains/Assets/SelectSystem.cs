c3a0728f7e1334d50df42b27bff02edf06f72a1a
Mr-JK4649 <ryouga>
2021-05-20 17:18:25 +0900

    セレクトのゲーム終了メニューのやつ

--- Treasure of Time Remains/Assets/SelectSystem.cs
+++ Treasure of Time Remains/Assets/SelectSystem.cs
@@ -8,6 +8,22 @@ public class SelectSystem : MonoBehaviour
     [SerializeField] private Text highScore;
     [SerializeField] private Text Level;
     [SerializeField] private GameObject[] buttons;
+    [SerializeField] private GameObject gameEndMenuObj;
+    [SerializeField] private GameObject firstSelectButtonEnd;
+
+    public bool gameEndMenu = false;
+    private Button[] button = new Button[5];
+    private GameObject oldSelectedButton;
+
+    private void Start()
+    {
+        gameEndMenuObj.SetActive(false);
+
+        for (int i = 0; i < buttons.Length; i++) {
+            button[i] = buttons[i].GetComponent<Button>();
+
+        }
+    }
 
     private void FixedUpdate()
     {
@@ -37,12 +53,54 @@ public class SelectSystem : MonoBehaviour
             }
         }
 
-    for (int i = 0; i < buttons.Length - 1; i++) 
+        for (int i = 0; i < buttons.Length - 1; i++) 
             {
                 if (go == buttons[i]) highScore.text = "ハイスコア：\n　　　" + ScoreManager.Instance.stage_Highscore[i].ToString();
             }
-        if (go == buttons[buttons.Length-1]) highScore.text = "";
+            if (go == buttons[buttons.Length-1]) highScore.text = "";
 
-        Debug.Log(buttons.Length);
+
+        //ゲーム終了メニュー
+        if (Input.GetButtonDown("Cont_Start")) {
+            gameEndMenu = !gameEndMenu;
+
+            //ゲーム終了メニューを開いてる間、後ろのボタンの遷移昨日をなくす。
+            if (gameEndMenu)
+            {
+                oldSelectedButton = eSys.currentSelectedGameObject;
+                ButtonNavChange(true);
+                EventSystem.current.SetSelectedGameObject(firstSelectButtonEnd);
+            }
+            else
+            {
+                ButtonNavChange(false);
+                EventSystem.current.SetSelectedGameObject(oldSelectedButton);
+            }
+
+        }
+        //if (Input.GetButtonUp("Cont_Start")) flg = true;
+    }
+
+    private void ButtonNavChange(bool t_f) {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            var nav = button[i].navigation;
+            if (t_f) nav.mode = Navigation.Mode.None;
+            else nav.mode = Navigation.Mode.Vertical;
+            button[i].navigation = nav;
+        }
+
+        gameEndMenuObj.SetActive(t_f);
+    }
+
+    public void GameEnd(bool y_n) {
+
+        if (y_n)//終了する
+        {
+            Application.Quit();
+        }
+        else { //終了しない
+            ButtonNavChange(false);
+        }
     }
 }
