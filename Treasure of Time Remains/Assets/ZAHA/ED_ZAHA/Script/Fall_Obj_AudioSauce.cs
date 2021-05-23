a540947520a30bf2140c7b2289ebcb4b311fefa3
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-05-23 20:46:40 +0900

    SE追加 END画面の

---
+++ Treasure of Time Remains/Assets/ZAHA/ED_ZAHA/Script/Fall_Obj_AudioSauce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall_Obj_AudioSauce : MonoBehaviour
+{
+    AudioSource myAudio;
+
+    private void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "FallObj")
+        {
+            myAudio.Play();
+        }
+    }
+}
