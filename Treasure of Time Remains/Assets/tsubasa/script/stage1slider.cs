b0c330fdde4fce64b134d983b9e2f66a379da19d
安藤翼 <tubasa-ando-19h@stu.kbc.ac.jp>
2021-05-26 15:09:12 +0900

    途中２

--- Treasure of Time Remains/Assets/tsubasa/script/stage1slider.cs
+++ Treasure of Time Remains/Assets/tsubasa/script/stage1slider.cs
@@ -1,32 +1,78 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class stage1slider : MonoBehaviour
 {
-    Slider distanceSlider;
-    //float z = 
+    private float timeleft;
+    public bool startFlag = false;　//止めたいときはfalseを入れる
+    Slider _slider;
+    float _hp = 0;
 
-    // Use this for initialization
     void Start()
     {
-        distanceSlider = GetComponent<Slider>();
-
-        float Goal = 200f;
-        float Start = 0f;
-
-        //ゴール地点設定
-        distanceSlider.maxValue = Goal;
-
-        //スタート地点の設定
-        distanceSlider.value = Start;
-
-
+        // スライダーを取得する
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (startFlag)
+        {
+            //だいたい1秒ごとに処理を行う
+            timeleft -= Time.deltaTime;
+            if (timeleft <= 0.0)
+            {
+                timeleft = 1.0f;
+                _hp += 1f;
+                if (_hp > 20)
+                {
 
+                    //以下の記述だと元に戻る。他の処理をしたいときはここに書く
+                    _hp = 0;
+                }
+                // HPゲージに値を設定
+                _slider.value = _hp;
+            }
+        }
     }
 
 }
+//    float hp = 20;
+
+//    [SerializeField]
+//    private int minute;
+//    [SerializeField]
+//    private float seconds;
+//    //前のUpdateの時の秒数
+//    private float oldSeconds;
+
+//    Vector3 tmp = GameObject.Find("ThirdPersonController").transform.position;
+//    //float z = tmp.z;
+
+//    Slider hpSlider;
+
+//    // Use this for initialization
+//    void Start()
+//    {
+//        minute = 0;
+//        seconds = 0f;
+//        oldSeconds = 0f;
+
+//        hpSlider = GetComponent<Slider>();
+//        hpSlider.maxValue = hp;
+//        hpSlider.value = 0;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        seconds += Time.deltaTime;
+//        if (minute == 5f)
+//        {
+//            hp += 1f;
+//            hpSlider.value = hp;
+//        }
+//    }
+//}
