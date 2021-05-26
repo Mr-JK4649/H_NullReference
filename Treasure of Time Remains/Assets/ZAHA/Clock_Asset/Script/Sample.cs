69da772b6aa3b46ab65eb3ac3822a583ca1d2f12
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-05-26 17:10:02 +0900

    seiyaステージ時計 effect追加

--- Treasure of Time Remains/Assets/ZAHA/Clock_Asset/Script/Sample.cs
+++ Treasure of Time Remains/Assets/ZAHA/Clock_Asset/Script/Sample.cs
@@ -4,105 +4,105 @@ using UnityEngine;
 
 public class Sample : MonoBehaviour
 {
-	//UI_Orb_prefab ui_orb;
-	//[SerializeField]
-	//GameObject UI_Orb;
-	[SerializeField] private ClockGauge clockGauge;
+    //UI_Orb_prefab ui_orb;
+    //[SerializeField]
+    //GameObject UI_Orb;
+    [SerializeField] private ClockGauge clockGauge;
 
-	[SerializeField] GameObject clock_anim_obj;
-	Clock_Animation clock_animation;
-
-
-	[SerializeField]
-	Camera _camera3d;
-	[SerializeField]
-	Camera _camera2d;
-	[SerializeField]
-	Transform[] _2dEffectTransforms;
-	[SerializeField]
-	Transform _Player;
-	[SerializeField]
-	RectTransform ClockUI;
+    [SerializeField] GameObject clock_anim_obj;
+    Clock_Animation clock_animation;
 
 
-	private const float Recovery = 0.2f;
+    [SerializeField]
+    Camera _camera3d;
+    [SerializeField]
+    Camera _camera2d;
+    [SerializeField]
+    Transform[] _2dEffectTransforms;
+    [SerializeField]
+    Transform _Player;
+    [SerializeField]
+    RectTransform ClockUI;
 
-	public bool effectStatus;//effect状態フラグ
 
-	float _footerUiPlaneDistanceFromCamera = 2f;
+    private const float Recovery = 0.2f;
 
-	float _effectDuration = 1f;
-	bool _useBezier = true;
+    public bool effectStatus;//effect状態フラグ
 
-	// エフェクト関連
-	class Effect
-	{
-		public Transform transform;
-		public Transform srcTransform;
-		public Transform dstTransform;
-		public float time;
-		public float uiPlaneDistanceFromCamera;
+    float _footerUiPlaneDistanceFromCamera = 2f;
 
-		public bool animationflg;
-	}
+    float _effectDuration = 1f;
+    bool _useBezier = true;
 
-	Effect[] _2dEffects;
+    // エフェクト関連
+    class Effect
+    {
+        public Transform transform;
+        public Transform srcTransform;
+        public Transform dstTransform;
+        public float time;
+        public float uiPlaneDistanceFromCamera;
 
-	int _2dEffectIndex;
+        public bool animationflg;
+    }
 
-	void Start()
-	{
-		//_2dEffectTransforms = new Transform[10];
+    Effect[] _2dEffects;
 
-		//ui_orb = UI_Orb.GetComponent<UI_Orb_prefab>();
+    int _2dEffectIndex;
 
-		//for (int i = 0; i < 10; i++)
-		//{
-  //          _2dEffectTransforms[i] = ui_orb.orb_clone[i].gameObject.GetComponent<Transform>();
-  //          Debug.Log("10回");
-		//}
+    void Start()
+    {
+        //_2dEffectTransforms = new Transform[10];
 
-		//instance作成
-		_2dEffects = new Effect[_2dEffectTransforms.Length];
+        //ui_orb = UI_Orb.GetComponent<UI_Orb_prefab>();
 
-		clock_animation = clock_anim_obj.GetComponent<Clock_Animation>();
-		//長さ文for文回す
-		for (int i = 0; i < _2dEffects.Length; i++)
-		{
-			_2dEffects[i] = new Effect();
+        //for (int i = 0; i < 10; i++)
+        //{
+        //          _2dEffectTransforms[i] = ui_orb.orb_clone[i].gameObject.GetComponent<Transform>();
+        //          Debug.Log("10回");
+        //}
+
+        //instance作成
+        _2dEffects = new Effect[_2dEffectTransforms.Length];
+
+        clock_animation = clock_anim_obj.GetComponent<Clock_Animation>();
+        //長さ文for文回す
+        for (int i = 0; i < _2dEffects.Length; i++)
+        {
+            _2dEffects[i] = new Effect();
             _2dEffects[i].transform = _2dEffectTransforms[i];
-			_2dEffects[i].transform.gameObject.SetActive(false);
-		}
-	}
+            _2dEffects[i].transform.gameObject.SetActive(false);
+        }
+    }
 
-	void Update()
-	{
-		//     for (int i = 0; i < _2dEffects.Length; i++)
-		//     {
-		//Debug.Log(_2dEffects[i].animationflg);
-		//if (_2dEffects[i].transform.gameObject.activeInHierarchy)
-		//         {
-		//	_2dEffects[i].animationflg = true;
+    void Update()
+    {
+        //     for (int i = 0; i < _2dEffects.Length; i++)
+        //     {
+        //Debug.Log(_2dEffects[i].animationflg);
+        //if (_2dEffects[i].transform.gameObject.activeInHierarchy)
+        //         {
+        //	_2dEffects[i].animationflg = true;
 
-		//	if (_2dEffects[i].animationflg && !_2dEffects[i].transform.gameObject.activeInHierarchy)
-		//	{
-		//		Debug.Log("終点");
-		//	}
-		//         }
-		//         else
-		//         {
-		//	//_2dEffects[i].animationflg = false;
-		//}
-		//     }
+        //	if (_2dEffects[i].animationflg && !_2dEffects[i].transform.gameObject.activeInHierarchy)
+        //	{
+        //		Debug.Log("終点");
+        //	}
+        //         }
+        //         else
+        //         {
+        //	//_2dEffects[i].animationflg = false;
+        //}
+        //     }
 
-		//Debug.Log(_2dEffects[i].transform.gameObject.name + _2dEffects[i].transform.gameObject.activeInHierarchy);
+        //Debug.Log(_2dEffects[i].transform.gameObject.name + _2dEffects[i].transform.gameObject.activeInHierarchy);
 
 
-		
+
         float dt = Time.deltaTime;
 
-		if (effectStatus)
-		{
+        if (effectStatus)
+        {
 
             Transform transform3d = null;
             transform3d = _Player; //プレイヤーのトランスフォームを代入
@@ -112,75 +112,75 @@ public class Sample : MonoBehaviour
             float uiDistanceFromCamera = 0f;
 
             transform2d = ClockUI; //UIのトランスフォームを代入
-			uiDistanceFromCamera = _footerUiPlaneDistanceFromCamera;//
+            uiDistanceFromCamera = _footerUiPlaneDistanceFromCamera;//
 
-			// 3D→2Dに変換 //動かしている
-			EmitEffect3dTo2d(transform3d, transform2d, uiDistanceFromCamera, _2dEffectIndex);
-			
-			//2Deffectカウントしていき
-			//2dのeffectが長さを超えたらリセット
+            // 3D→2Dに変換 //動かしている
+            EmitEffect3dTo2d(transform3d, transform2d, uiDistanceFromCamera, _2dEffectIndex);
 
-			_2dEffectIndex++;
-			if (_2dEffectIndex >= _2dEffects.Length)
-			{
-				_2dEffectIndex = 0;
-			}
-			effectStatus = false;
-		}
-		
-		UpdateEffects(dt);
+            //2Deffectカウントしていき
+            //2dのeffectが長さを超えたらリセット
+
+            _2dEffectIndex++;
+            if (_2dEffectIndex >= _2dEffects.Length)
+            {
+                _2dEffectIndex = 0;
+            }
+            effectStatus = false;
+        }
+
+        UpdateEffects(dt);
     }
 
-	void EmitEffect3dTo2d(Transform src, Transform dst, float uiPlaneDistanceFromCamera, int index)
-	{
-		var effect = _2dEffects[index];
-		effect.time = 0f;
-		effect.transform.gameObject.SetActive(true);
-		effect.uiPlaneDistanceFromCamera = uiPlaneDistanceFromCamera;
-		effect.srcTransform = src;
-		effect.dstTransform = dst;
+    void EmitEffect3dTo2d(Transform src, Transform dst, float uiPlaneDistanceFromCamera, int index)
+    {
+        var effect = _2dEffects[index];
+        effect.time = 0f;
+        effect.transform.gameObject.SetActive(true);
+        effect.uiPlaneDistanceFromCamera = uiPlaneDistanceFromCamera;
+        effect.srcTransform = src;
+        effect.dstTransform = dst;
 
-	}
+    }
 
-	void UpdateEffects(float dt)
-	{
-		for (int i = 0; i < _2dEffects.Length; i++)
-		{
+    void UpdateEffects(float dt)
+    {
+        for (int i = 0; i < _2dEffects.Length; i++)
+        {
 
-			UpdateEffect2D(_2dEffects[i], dt);
+            UpdateEffect2D(_2dEffects[i], dt);
         }
 
     }
 
-	// 2D要素が3D空間にあったとしたらどこにあるか、を算出する
-	static void Calc3dWorldPositionOfUi(
+    // 2D要素が3D空間にあったとしたらどこにあるか、を算出する
+    static void Calc3dWorldPositionOfUi(
 
-		out Vector3 worldPositionOut,
-		Transform uiTransform,
-		Camera camera2d,
-		Camera camera3d,
-		float uiPlaneDistanceFromCamera)
-	{
-		var worldPos2d = uiTransform.position; // 2D世界おけるワールド座標を得る
-		var screenPos = camera2d.WorldToScreenPoint(worldPos2d); // スクリーン座標に変換
-		var ray = camera3d.ScreenPointToRay(screenPos); // 3D世界におけるカメラから出たレイに変換
-		var cosine = Vector3.Dot(ray.direction, camera3d.transform.forward)
-			/ camera3d.transform.forward.magnitude; // ray.directionの長さは1固定なので割らずに済む
-		var distance = uiPlaneDistanceFromCamera / cosine;
-		worldPositionOut = camera3d.transform.position + (ray.direction * distance);
+        out Vector3 worldPositionOut,
+        Transform uiTransform,
+        Camera camera2d,
+        Camera camera3d,
+        float uiPlaneDistanceFromCamera)
+    {
+        var worldPos2d = uiTransform.position; // 2D世界おけるワールド座標を得る
+        var screenPos = camera2d.WorldToScreenPoint(worldPos2d); // スクリーン座標に変換
+        var ray = camera3d.ScreenPointToRay(screenPos); // 3D世界におけるカメラから出たレイに変換
+        var cosine = Vector3.Dot(ray.direction, camera3d.transform.forward)
+            / camera3d.transform.forward.magnitude; // ray.directionの長さは1固定なので割らずに済む
+        var distance = uiPlaneDistanceFromCamera / cosine;
+        worldPositionOut = camera3d.transform.position + (ray.direction * distance);
 
-	}
+    }
 
     void UpdateEffect2D(Effect effect, float dt)
     {
-		
-		if (!effect.transform.gameObject.activeSelf)
+
+        if (!effect.transform.gameObject.activeSelf)
         {
             return;
         }
 
-		// 開始点のワールド座標を得る
-		var srcWorldPos = effect.srcTransform.position;
+        // 開始点のワールド座標を得る
+        var srcWorldPos = effect.srcTransform.position;
 
         // 目標点のワールド座標を求める
         Vector3 dstWorldPos;
@@ -194,9 +194,9 @@ public class Sample : MonoBehaviour
         // 正規化時刻を求める
         var t = effect.time / _effectDuration;
         t *= t; // 加速した方がかっこいいので加速させてみる。このサンプルの本質には関係ない。
-				// 補間する
+                // 補間する
 
-		Debug.Log(t);
+        Debug.Log(t);
         if (t >= 1)
         {
 
@@ -243,11 +243,11 @@ public class Sample : MonoBehaviour
     }
 
     static void Bezier(out Vector3 pOut, ref Vector3 p0, ref Vector3 p1, ref Vector3 controlPoint, float t)
-	{
-		pOut = p1 - (controlPoint * 2f) + p0;
-		pOut *= t;
-		pOut += (controlPoint - p0) * 2f;
-		pOut *= t;
-		pOut += p0;
-	}
+    {
+        pOut = p1 - (controlPoint * 2f) + p0;
+        pOut *= t;
+        pOut += (controlPoint - p0) * 2f;
+        pOut *= t;
+        pOut += p0;
+    }
 }
---
+++ Treasure of Time Remains/Assets/ZAHA/ED_ZAHA/Script/Camera_gap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Camera : MonoBehaviour
+{
+    GameObject scen_camera;
+    Vector3 init_pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        init_pos = scen_camera.gameObject.transform.position;//カメラの初期座標保存
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        //scenで落ちてきたオブジェクトのタグがFallObjだったら
+        if(other.gameObject.tag == "FallObj")
+        {
+            scen_camera.transform.position = init_pos;
+        }
+    }
+}
